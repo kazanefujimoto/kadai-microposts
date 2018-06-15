ee7ef03bf732296912d85f7ab3ad46ab1589cb64
kazane.fujimoto <kazane.fujimoto@rakuten.com>
2018-06-15 04:15:11 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -13,17 +13,14 @@ class MicropostsController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
                 'microposts' => $microposts,
             ];
-            $data += $this->counts($user);
-            return view('users.show', $data);
-        }else {
-            return view('welcome');
         }
+        return view('welcome', $data);
     }
     public function store(Request $request)
     {
@@ -37,6 +34,9 @@ class MicropostsController extends Controller
 
         return redirect()->back();
     }
+    
+    
+    
     public function destroy($id)
     {
         $micropost = \App\Micropost::find($id);
@@ -47,5 +47,7 @@ class MicropostsController extends Controller
 
         return redirect()->back();
     }
+    
+    
 }
 
